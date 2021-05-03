6402dc6da0d8594fdf311ad96d829e94e160213e
Keon Pourboghrat <kekons4@gmail.com>
2021-05-03 15:30:42 -0400

    added avatar image functionality

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Category } = require('../models');
 
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         const postData = await Post.findAll({ 
             include: [
@@ -14,14 +14,19 @@ router.get('/', async (req, res) => {
                     model: Category,
                     attributes: ["name"]
                 }
-            ]
+            ],
+            where: {
+                category_id: req.params.id
+            }
         });
 
         const posts = postData.map((post) => post.get({ plain: true }));
 
         res.render('category', {
             posts,
-            logged_in: req.session.logged_in
+            logged_in: req.session.logged_in,
+            avatar_url: req.session.avatar_url,
+            category_name: posts[0].category.name
         });
     } catch (err) {
         res.status(500).json(err);
