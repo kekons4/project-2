4e4ca7042ac20c99385c742c27e422f64694b684
Keon Pourboghrat <kekons4@gmail.com>
2021-05-03 20:19:02 -0400

    making more changes to categoies page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in =true;
-            req.session.avatar_url = "/images/soccerball.jpg";
+            req.session.avatar_url = userData.avatar_url;
 
             res.json({ user: userData, message: 'You are now logged in!' });
         });
--- controllers/categories.js
+++ controllers/categories.js
@@ -2,35 +2,78 @@ const router = require('express').Router();
 const { User, Post, Category } = require('../models');
 
 
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const postData = await Post.findAll({ 
+        const categoryData = await Category.findAll();
+        const categories = categoryData.map((category) => {
+            category.get({plain: true});
+        });
+        res.render('categories', {categories, avatar_url: req.session.avatar_url, logged_in: req.session.logged_in}); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:name', async (req, res) => {
+    try {
+        const categoryData = await Post.findAll({ 
             include: [
-                { 
-                    model: User,
-                    attributes: ["username", "avatar_url"]  
-                },
                 {
                     model: Category,
                     attributes: ["name"]
+                },
+                { 
+                    model: User,
+                    attributes: ["username", "avatar_url"]  
                 }
             ],
             where: {
-                category_id: req.params.id
+                name: req.params.name
             }
         });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+        
+        const category = categoryData.get({ plain: true });
 
         res.render('category', {
-            posts,
+            category,
             logged_in: req.session.logged_in,
-            avatar_url: req.session.avatar_url,
-            category_name: posts[0].category.name
+            avatar_url: req.session.avatar_url
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const postData = await Post.findAll({ 
+//             include: [
+//                 { 
+//                     model: User,
+//                     attributes: ["username", "avatar_url"]  
+//                 },
+//                 {
+//                     model: Category,
+//                     attributes: ["name"]
+//                 }
+//             ],
+//             where: {
+//                 category_id: req.params.id
+//             }
+//         });
+
+//         const posts = postData.map((post) => post.get({ plain: true }));
+
+//         res.render('category', {
+//             posts,
+//             logged_in: req.session.logged_in,
+//             avatar_url: req.session.avatar_url,
+//             category_name: posts[0].category.name
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
