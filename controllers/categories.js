3fea93cb16657eb3c5585d44958cac41b17b4a5e
Keon Pourboghrat <kekons4@gmail.com>
2021-05-03 22:51:24 -0400

    added categories page

--- controllers/categories.js
+++ controllers/categories.js
@@ -14,66 +14,66 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:name', async (req, res) => {
-    try {
-        const categoryData = await Post.findAll({ 
-            include: [
-                {
-                    model: Category,
-                    attributes: ["name"]
-                },
-                { 
-                    model: User,
-                    attributes: ["username", "avatar_url"]  
-                }
-            ],
-            where: {
-                name: req.params.name
-            }
-        });
-
-        
-        const category = categoryData.get({ plain: true });
-
-        res.render('category', {
-            category,
-            logged_in: req.session.logged_in,
-            avatar_url: req.session.avatar_url
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// router.get('/:id', async (req, res) => {
+// router.get('/:name', async (req, res) => {
 //     try {
-//         const postData = await Post.findAll({ 
+//         const categoryData = await Post.findAll({ 
 //             include: [
-//                 { 
-//                     model: User,
-//                     attributes: ["username", "avatar_url"]  
-//                 },
 //                 {
 //                     model: Category,
 //                     attributes: ["name"]
+//                 },
+//                 { 
+//                     model: User,
+//                     attributes: ["username", "avatar_url"]  
 //                 }
 //             ],
 //             where: {
-//                 category_id: req.params.id
+//                 name: req.params.name
 //             }
 //         });
 
-//         const posts = postData.map((post) => post.get({ plain: true }));
+        
+//         const category = categoryData.get({ plain: true });
 
 //         res.render('category', {
-//             posts,
+//             category,
 //             logged_in: req.session.logged_in,
-//             avatar_url: req.session.avatar_url,
-//             category_name: posts[0].category.name
+//             avatar_url: req.session.avatar_url
 //         });
 //     } catch (err) {
 //         res.status(500).json(err);
 //     }
 // });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findAll({ 
+            include: [
+                { 
+                    model: User,
+                    attributes: ["username", "avatar_url"]  
+                },
+                {
+                    model: Category,
+                    attributes: ["name"]
+                }
+            ],
+            where: {
+                category_id: req.params.id
+            }
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('category', {
+            posts,
+            logged_in: req.session.logged_in,
+            avatar_url: req.session.avatar_url,
+            category_name: posts[0].category.name
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
